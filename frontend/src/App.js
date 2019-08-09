3b1f3ea41b1c649f13ab77aaab698368d3444167
Anatoly <tolik_83@mail.ru>
2019-08-09 16:46:06 +0300

    Заголовок сайта

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import { Route, Switch } from 'react-router-dom';
+
 import Header from './Components/Header';
 import Content from './Components/Content';
 
@@ -8,7 +10,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Content />
+      <Switch>
+        <Route path='/' exact component={Content}/>
+      </Switch>
     </div>
   );
 }
--- frontend/src/Components/Header/index.js
+++ frontend/src/Components/Header/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Container, Row, Col, Nav, NavItem, NavLink  } from 'reactstrap';
-import logo from '../../img/logo.png'
+import { Container, Row, Col, Nav, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import logo from '../../img/logo.png';
+import mapMarker from '../../img/icons/map-marker.png';
+import clock from '../../img/icons/clock.png';
+import phone from '../../img/icons/phone.png';
+import './style.css';
 
-function Header() {
+function Header(props) {
     return (
         <Container>
             <Row>
@@ -10,21 +15,50 @@ function Header() {
                     <img src={logo} alt='логотип' />
                 </Col>
                 <Col>
-                    <Nav>
+                    <Nav className='main-menu'>
                         <NavItem>
-                            <NavLink href="#">Главная</NavLink>
+                            <NavLink exact to='/' activeClassName='active'>Главная</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="#">Услуги</NavLink>
+                            <NavLink to='/services' activeClassName='active'>Услуги</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="#">Специалисты</NavLink>
+                            <NavLink to='/doctors' activeClassName='active'>Специалисты</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink disabled href="#">Контакты</NavLink>
+                            <NavLink to='/contacts' activeClassName='active'>Контакты</NavLink>
                         </NavItem>
                     </Nav>
                     <hr />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <div style={{float: "left"}}>
+                        <img src={mapMarker} alt='mapMarker' />
+                    </div>
+                    <div style={{float: "left"}}>
+                        <h6>Ленина, 20</h6>
+                        <p>Волжск, Марий Эл</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div style={{float: "left"}}>
+                        <img src={clock} alt='clock' />
+                    </div>
+                    <div style={{float: "left"}}>
+                        <h6>Режим работы :</h6>
+                        <p>Пн-Пт: 8:00 - 20:00</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div style={{float: "left"}}>
+                        <img src={phone} alt='phone' />
+                    </div>
+                    <div style={{float: "left"}}>
+                        <h6>Телефон</h6>
+                        <p>+7 (906) 777 77 77</p>
+                    </div>
                 </Col>
             </Row>
         </Container>
