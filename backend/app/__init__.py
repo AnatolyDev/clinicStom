5eacea3c63d5c908cfe2b825bcbb2ece4f1b3dbc
Anatoly <tolik_83@mail.ru>
2019-09-06 11:29:39 +0300

    Настройка CORS в API

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 from .database import db
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(os.environ['APP_SETTINGS'])
 
     db.init_app(app)
